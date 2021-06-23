c4441b61daccfce27f26fec63ceffb8a30b8d83a
Isadora Soares <isadoragalvaoss@gmail.com>
2021-06-23 06:35:06 -0300

    Consumindo API da SpaceX na página Latest e Next

--- src/components/CardOne.js
+++ src/components/CardOne.js
@@ -1,27 +1,27 @@
-export const CardOne = () => (
+export const CardOne = ({launch}) => (
     <div className="col-md-12 ">
-            <div className="card ">
-            <h1>Nome</h1>
+        <div className="card ">
+            <h1>{launch.name}</h1>
             <div className="row">
               
-            <div className="col-6 ">
+                <div className="col-6 ">
 
-                <h6>Data</h6>
-                <h6>Detalhes</h6>
-                <h6>Sucesso ?</h6>
-                <h6>See more:</h6>
-                <a href="/"></a> 
-                <a href="/" >youtube</a> 
-                    <a className="ml-1" href="/">wikipedia </a>
+                    <h6>{Date(launch.date_utc)}</h6>
+                    <h6>{launch.details}</h6>
+                    <h6>{launch.success ? `Was successful`:`Was not successful`}</h6>
+                    <h6>See more:</h6>
+                    <a href={launch.links?.webcast}>youtube</a> 
+                    <a className="ml-5" href={launch.links?.wikipedia}>wikipedia </a>
+
+                </div>
+                <div className="col-6">
+                    <img className="card-img-top" src={launch.links?.patch?.small} alt=""/>
+                </div>
             </div>
-            <div className="col-6">
-        <img className="card-img-top" src="/" alt=""/>
-            </div>
+
         </div>
-
-            </div>
             
-        </div>
+    </div>
           
 );
 
--- src/containers/Next.js
+++ src/containers/Next.js
@@ -1,10 +1,18 @@
 import { CardOne } from "../components/CardOne";
+import { useState,useEffect } from  'react';
+
 
 function Next(){
-  
+    const[launches,setLaunches] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.spacexdata.com/v4/launches/next')
+        .then((response) => response.json())
+        .then(launches => {setLaunches(launches)});
+     } , [])
 
     return (
-      <CardOne />
+      <CardOne launch={launches}/>
     );
   }
   
