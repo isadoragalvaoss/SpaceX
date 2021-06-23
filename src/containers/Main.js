ccf7c76fe607813d14d7f6838c07e9c9bbfba898
Isadora Soares <isadoragalvaoss@gmail.com>
2021-06-23 06:10:28 -0300

    Consumindo API da SpaceX na página Main

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,11 +1,19 @@
 import { MainCmp } from '../components/Main';
+import { useState,useEffect} from  'react';
+
 
 function Main(){
-  
+    const [launches, setLaunches] = useState([]);
+
+    useEffect(() => {
+      fetch("https://api.spacexdata.com/v4/company")
+          .then(response => response.json())
+          .then(launches => { setLaunches(launches)});
+    } , [])
 
   return (
     <div>
-        <MainCmp />
+        <MainCmp launch={launches}/>
     </div>
   );
 }
