118be9408bb76b7a3f72bbf5b5cf913528447689
Isadora Soares <isadoragalvaoss@gmail.com>
2021-06-23 04:44:07 -0300

    Criando rotas para as páginas

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import './App.css';
 
 import { BrowserRouter as Router , Switch, Route } from "react-router-dom"
 import { Header } from './components/Header';
+import All from './containers/All';
+import Upcoming from './containers/Upcoming';
+import Past from './containers/Past';
+import Next from './containers/Next';
+import Latest from './containers/Latest';
+import Main from './containers/Main';
 
 function App(){
   return(
     <Router>
       <Header/>
-    
+    <div>
+    <Switch>
+          <Route exact path="/" component={Main}></Route>
+          <Route exact path="/all" component={All}></Route>
+          <Route exact path="/upcoming" component={Upcoming}></Route>
+          <Route exact path="/past" component={Past}></Route>
+          <Route exact path="/next" component={Next}></Route>
+          <Route exact path="/latest" component={Latest}></Route>
+        </Switch>
+    </div>
     </Router>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,16 @@ import logo from "../imgs/logo.png"
 export const Header = ({lauch}) => (
     <nav className="navbar navbar-expand-lg ">
       <div className="container-fluid">
-      <a className="navbar-brand " href="/" id="logo"><img src={logo} alt=""/></a>
+    <Link to="/" className="navbar-brand" id="logo"><img src={logo} alt=""/></Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div className="navbar-nav">
-    <Link to="/" className="nav-item nav-link pl-md-5">Next Launch</Link>
-    <Link to="/" className="nav-item nav-link pl-md-5">Upcoming Launches</Link>
-    <Link to="/" className="nav-item nav-link pl-md-5">Latest Launch</Link>
-    <Link to="/" className="nav-item nav-link pl-md-5">Past Launches</Link>
+    <Link to="/next" className="nav-item nav-link pl-md-5">Next Launch</Link>
+    <Link to="/upcoming" className="nav-item nav-link pl-md-5">Upcoming Launches</Link>
+    <Link to="/latest" className="nav-item nav-link pl-md-5">Latest Launch</Link>
+    <Link to="/past" className="nav-item nav-link pl-md-5">Past Launches</Link>
     </div>
   </div>
       </div>
